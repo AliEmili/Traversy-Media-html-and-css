693bf84f4273cd4558f440fe42aadf68256e08e9
Ali Emili <aliemili2013@yahoo.com>
2019-08-01 10:22:45 +0430

    Modifying

---
+++ 4.Fourth project/JavaScript/main.js
@@ -0,0 +1,31 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(e)
+{
+    e.preventDefault();
+    
+    if(nameInput.value =='' || emailInput.value == '')
+    {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please Enter All Fields';
+        
+        setTimeout(() => msg.remove(),3000);
+    }
+    else
+    {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        
+        userList.appendChild(li);
+        
+        // Clearing the fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
