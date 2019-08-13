031c33b11cc95f288b9216bb4ebd791c9185e1cc
Ali Emili <aliemili2013@yahoo.com>
2019-08-13 17:34:57 +0430

    initial commit

---
+++ 7.Seventh project/javascript/main.js
@@ -0,0 +1,10 @@
+// Animate smooth scroll
+$('#view-work').on('click', function () {
+    const images = $('#images').position().top;
+
+    $('html,body').animate({
+            scrollTop: images
+        },
+        900
+    );
+});
